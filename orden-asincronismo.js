4721b076aea644ba49d40210a595d2f6dbde9d38
developer-jesus-github <developerjesus@hotmail.com>
2021-01-08 15:54:03 -0600

    Se agregó archivo Manejando el orden y el asincronismo en JavaScript

---
+++ orden-asincronismo.js
@@ -0,0 +1,28 @@
+const API_URL = 'https://swapi.dev/api/'
+const PEOPLE_URL = 'people/:id'
+const opts ={ crossDomain: true}
+
+const onResponse = function (data) {
+    console.log(`Hola, yo soy ${data.name}`)
+}
+
+const obtenerPersonaje = (id, callback) => {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+    $.get( url , opts,onResponse)
+
+    if(callback){
+        callback()
+    }
+} 
+
+
+
+obtenerPersonaje(1 , function (){
+    obtenerPersonaje(2, function () {
+        obtenerPersonaje(3,function(){
+            obtenerPersonaje(4,function(){
+                obtenerPersonaje(5)
+            })
+        })
+    })
+})
